bd2082bc55b796646f0957710479a9000960d3c9
gaber <bertongabri@gmail.com>
2021-04-10 20:09:52 +0200

    Aggiunto file standalone che scarica SVOX da google drive

---
+++ src/download_and_unzip_svox.py
@@ -0,0 +1,37 @@
+
+import os
+import shutil
+import requests
+
+def download_file_from_google_drive(zip_destination="svox.zip",
+                                    file_id="1S3RreRlBL7c7QSCf6NglGTAUVdg_5bZe"):
+    def get_confirm_token(response):
+        for key, value in response.cookies.items():
+            if key.startswith('download_warning'):
+                return value
+        return None
+    def save_response_content(response, zip_destination):
+        CHUNK_SIZE = 32768
+        with open(zip_destination, "wb") as f:
+            for chunk in response.iter_content(CHUNK_SIZE):
+                if chunk:  # Filter out keep-alive new chunks
+                    f.write(chunk)
+    URL = "https://docs.google.com/uc?export=download"
+    session = requests.Session()
+    response = session.get(URL, params={'id': file_id}, stream=True)
+    token = get_confirm_token(response)
+    if token:
+        params = {'id': file_id, 'confirm': token}
+        response = session.get(URL, params=params, stream=True)
+    save_response_content(response, zip_destination)    
+
+def download_and_unzip_svox(dataset_destination="datasets",
+                            zip_destination="svox.zip", delete_zip=True):
+    download_file_from_google_drive(zip_destination=zip_destination)
+    shutil.unpack_archive(zip_destination, dataset_destination)
+    if delete_zip:
+        os.remove(zip_destination)
+
+if __file__ == "__main__":
+    download_and_unzip_svox()
+
