45b5099419762ef9cb67f479f87f4eb880e094cd
Gaber <s252805@studenti.polito.it>
2021-07-05 21:28:12 +0200

    Managed to save figure. Style transfer not working

--- src/fda_util.py
+++ src/fda_util.py
@@ -24,21 +24,19 @@ def src_to_target_np(src_img, trg_img, beta = 0.1):
     trg_ft = np.fft.fft2(trg_img)
 
     # Separate amplitudes and phases
-    src_amp = np.abs(src_ft)
-    src_phase = np.angle(src_ft)
-    trg_amp = np.abs(trg_ft)
-    trg_amp = np.angle(trg_ft)
+    src_amp, src_phase = np.abs(src_ft), np.angle(src_ft)
+    trg_amp, trg_phase = np.abs(trg_ft), np.angle(trg_ft)
 
     replaced_src_amp = replace_low_freq_np(src_amp, trg_amp, beta)
 
     # Recompose source FT with replace low level frequencies
-    src_ft = replaced_src_amp * np.exp(1j*2*np.pi*src_phase)
+    src_ft = replaced_src_amp * np.exp(1j*src_phase)
     
 
     # Inverse 2D FT
-    # Loro lo fanno diverso... E in piu mi rimane una parte immaginaria residua
-    src_to_trg_img = np.fft.ifft2(src_ft)
-    print(src_to_trg_img)
+    # Loro usano ifftshift - testo differenza.
+    # Con ifftshift permane una parte complessa, dubito che possa essere compatibile con .jpg
+    src_to_trg_img = np.real(np.fft.ifft2(src_ft, axes = (-2, -1))) # np.real rimuove parte complessa, che è odg 1E-16
 
     return src_to_trg_img
 
@@ -73,19 +71,22 @@ def replace_low_freq_np(src_amp, trg_amp, beta):
 if __name__ == '__main__':
 
     # Run test
-    src_img_path = '/home/valerio/francesco/adageo-WACV2021/src/datasets/svox/examples/RobotCar_snow.jpg'
-    trg_img_path = '/home/valerio/francesco/adageo-WACV2021/src/datasets/svox/examples/RobotCar_rain.jpg'
+    src_img_path = '/home/valerio/francesco/adageo-WACV2021/src/datasets/svox/examples/RobotCar_rain.jpg'
+    trg_img_path = '/home/valerio/francesco/adageo-WACV2021/src/datasets/svox/examples/RobotCar_snow.jpg'
 
     # Read images in numpy array
     src_img = np.array(Image.open(src_img_path), dtype=float)
     trg_img = np.array(Image.open(trg_img_path), dtype=float)
 
     # Pass images to the function
-    src_to_trg_img = src_to_target_np(src_img, trg_img)
+    src_to_trg_img = src_to_target_np(src_img, trg_img, beta = 0.5)
 
     # Plot source image before and after
-    # fig, ax = plt.subplots(1, 2)
-    # ax[0].imshow(src_img)
-    # ax[1].imshow(src_to_trg_img)
-    
-    # plt.show()
+    fig, ax = plt.subplots(1, 2)
+    ax[0].imshow(src_img)
+    ax[1].imshow(src_to_trg_img)
+
+    np.save("../tmp/test_src.npy", src_img.astype(int))
+    np.save("../tmp/test_src2trg.npy", src_to_trg_img.astype(int))
+
+    fig.savefig("../tmp/test_src.jpg")
