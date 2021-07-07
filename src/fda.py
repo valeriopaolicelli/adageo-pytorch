fdc9f0dcf2fee5d47d7689914285697bd12a0243
Gaber <s252805@studenti.polito.it>
2021-07-07 16:23:47 +0200

    Validation of beta working

--- src/fda.py
+++ src/fda.py
@@ -7,6 +7,7 @@ from PIL import Image
 from fda_util import FDA_source_to_target_np, parse_arguments, scale
 
 ######################################### SETUP #########################################
+start = time.time()
 args = parse_arguments()
 root = args.dataset_root
 train_q = os.path.join(root, args.train_q)
@@ -14,6 +15,7 @@ val_q = os.path.join(root, args.val_q)
 src_q_paths = [train_q, val_q]
 targets = ["night", "overcast", "rain", "snow", "sun"]
 
+# TODO: remove target_size and directly perform transformation over all sizes.
 assert args.target_size in ['1', '5', '20', '50', 'ALL'],\
  f"Expected target size to be in ['1', '5', '20', '50', 'ALL'], but got {args.target_size}"
 
@@ -31,12 +33,11 @@ if not args.val_beta:
             source_images = sorted(os.listdir(src_q_path)) # sorted makes result reproducible
 
             # output_folder: root + "/train" or "val" + "/queries/query_pseudo_{target}"
-            output_path = src_q_path + "_" + "pseudo" + "_" + target
+            output_path = src_q_path + "_" + "pseudo" + "_" + target + "_" + args.target_size + "shots"
             if not os.path.isdir(output_path):
                 os.mkdir(output_path)
 
             # Map each source image into target domain
-            # for source_img in os.listdir(src_q_path):
             for i in range(len(source_images)):
                 src_img = Image.open(os.path.join(src_q_path, source_images[i]))
                 trg_img = Image.open(os.path.join(trg_q_path, random.choice(target_imgs)))
@@ -45,20 +46,24 @@ if not args.val_beta:
 
                 src_in_trg_img = FDA_source_to_target_np(src_img_np, trg_img_np, args.beta)
 
-                # Store the transformed image
+                # Store the transformed image. IMPORTANT: Scale between [0, 255] to remove numerical artifacts
                 Image.fromarray(scale(src_in_trg_img)).save(os.path.join(output_path, source_images[i]))
 
-# Choose beta
+# Tune beta
 else:
     valid_imgs_path = os.path.join(root, "beta")
+    beta_src_path = os.path.join(valid_imgs_path, "source")
     if not os.path.isdir(valid_imgs_path):
         os.mkdir(valid_imgs_path)
+        os.mkdir(beta_src_path)
 
-    beta_vals = [0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.2, 0.5, 0.99]
-    val_size = 5 # compare results on 5 source images
+
+    beta_vals = [0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.2]
+    val_size = 5 # compare results on val_size num of source images
     src_q_path = train_q
 
     for beta in beta_vals:
+        print(f"Processing images with beta {beta}...")
         for target in targets:
             # Target queries folder
             trg_q_path = train_q + "_" + target
@@ -73,13 +78,18 @@ else:
                 os.mkdir(output_path)
 
             # Map each source image into target domain
-            for i in range(1):
+            for i in range(val_size):
                 src_img = Image.open(os.path.join(src_q_path, source_images[i]))
                 trg_img = Image.open(os.path.join(trg_q_path, random.choice(target_imgs)))
                 src_img_np = np.asarray(src_img, np.float32)
                 trg_img_np = np.asarray(trg_img, np.float32)
 
-                src_in_trg_img = FDA_source_to_target_np(src_img_np, trg_img_np, args.beta)
+                src_in_trg_img = FDA_source_to_target_np(src_img_np, trg_img_np, beta)
 
-                # Store the transformed image
+                # Store the transformed image. IMPORTANT: Scale between [0, 255] to remove numerical artifacts
                 Image.fromarray(scale(src_in_trg_img)).save(os.path.join(output_path, source_images[i]))
+
+                if not os.path.exists(os.path.join(beta_src_path, source_images[i])):
+                    src_img.save(os.path.join(beta_src_path, source_images[i]))
+
+    print(f"Time for beta validation on {val_size*len(beta_vals)*len(targets)} images: {time.time() - start}s")
