3144fd2be5f203a86f86aba5397667242ec8e488
Francesco Montagna <francesco.montagna997@gmail.com>
2021-07-23 09:49:31 +0200

    code for experiments on beta

---
+++ src/beta_exp.py
@@ -0,0 +1,44 @@
+
+import os
+import sys
+import time
+
+folder = "/home/francescom/adageo-WACV2021/src"
+if not os.path.abspath(os.curdir) == folder:
+    sys.exit()
+
+print("here")
+os.makedirs(f"{folder}/jobs", exist_ok=True)
+os.makedirs(f"{folder}/out_job", exist_ok=True)
+
+for seed in range(3):
+    for target_domain in ["snow", "rain", "overcast"]:
+        for shots in ["5"]:
+            for beta in ["0.01"]:
+                print(f"queries_{target_domain}_pseudo_{shots}_{beta}")
+                exp_name = f"{target_domain}_{beta}_{shots}"
+                exp_name_seed = f"{exp_name}_{beta}_{seed}"
+                filename = f"{folder}/jobs/{exp_name_seed}.job"
+                content = ("" +
+                "#!/bin/bash \n" +
+                f"#SBATCH --job-name={exp_name_seed} \n" +
+                "#SBATCH --gres=gpu:1 \n" +
+                "#SBATCH --cpus-per-task=3 \n" +
+                "#SBATCH --mem=30GB \n" +
+                "#SBATCH --time=48:00:00 \n" +
+                f"#SBATCH --output={folder}/out_job/out_{exp_name_seed}.txt \n" +
+                f"#SBATCH --error={folder}/out_job/err_{exp_name_seed}.txt \n" +
+                "ml purge \nml Python/3.6.6-gomkl-2018b \n" +
+                "source /home/gabriele/iccv_tutto/myenv/bin/activate \n" +
+                f"python {folder}/main.py " +
+                f"--seed={seed} --dataset_root=/home/francescom/adageo-WACV2021/src/datasets/svox/images " +
+                f"--test_g=test/gallery --test_q=test/queries_{target_domain} --grl --attention " +
+                f"--grl_datasets=train/queries+test/queries_{target_domain}+train/queries_{target_domain}_pseudo_{shots}_{beta} " +
+                f"--val_q=val/queries_{target_domain}_pseudo_{shots}_{beta}\n")
+            
+                with open(filename, "w") as file:
+                    _ = file.write(content)
+            
+                _ = os.system(f"sbatch {filename}")
+
+                time.sleep(10)
